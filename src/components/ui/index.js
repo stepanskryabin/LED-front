d9f5e41ae2fa7c612e2099c00dfdaeb6abb814a9
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-07-06 14:08:04 +0300

    add navbar

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,11 +3,13 @@ import mainInput from "@/components/ui/mainInput";
 import mainDialog from "@/components/ui/mainDialog";
 import mainSelect from "@/components/ui/mainSelect";
 import mainPag from "@/components/ui/mainPag";
+import mainNavbar from "@/components/ui/mainNavbar";
 
 export default [
     mainButton,
     mainInput,
     mainDialog,
     mainSelect,
-    mainPag
+    mainPag,
+    mainNavbar
 ]
