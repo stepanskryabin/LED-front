92f41720c2909aea2db53aab5684cecf17e7899c
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-07-06 14:07:23 +0300

    add navbar

---
+++ src/components/ui/mainNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <dev @click="$router.push('/')" class="link">Сайт</dev>
+    <div class="link-btn">
+      <button @click="$router.push('/post')" class="link">Посты</button>
+      <button @click="$router.push('/about')" class="link">О нас</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainNavbar"
+}
+</script>
+
+<style scoped>
+.navbar {
+  margin: 0;
+  height: 50px;
+  background: green;
+  display: flex;
+  align-items: center;
+}
+.link {
+  border: solid black;
+  margin: 10px
+}
+.link-btn {
+  margin-left: auto;
+}
+</style>
